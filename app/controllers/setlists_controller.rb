ac9d226beed9cf7f9f3fdde276f52a9ed2588d3b
Owen Klumb <owen.klumb@gmail.com>
2017-07-31 14:58:41 +0900

    basic homepage design

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -1,5 +1,16 @@
 class SetlistsController < ApplicationController
+  before_action :set_setlist, only: [:show, :edit, :update, :destory]
   def new
     @setlist = Setlist.new
   end
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_setlist
+    @setlist = Setlist.find(params[:id])
+  end
+  def setlist_params
+      params.require(:setlist).permit(:name)
+    end
+
 end
