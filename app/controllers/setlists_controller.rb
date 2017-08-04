e1181554e29972303f5b02834381571750284908
Owen Klumb <owen.klumb@gmail.com>
2017-08-04 15:54:11 +0900

    album list added

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -6,7 +6,6 @@ class SetlistsController < ApplicationController
 
     artist_search = RSpotify::Artist.search('Black Sabbath')
     artist = artist_search.first
-    p artist
     @albums = artist.albums(limit: 50, country: current_user.market)
 
   end
